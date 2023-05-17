bb886eecd6f94e19bff7cfd21381fd5396a7db60
Mattie Barton <maitlandbarton@gmail.com>
2023-05-17 18:31:03 +0200

    remove insta slide images

--- src/data/instaSlideData.js
+++ src/data/instaSlideData.js
@@ -1,25 +1,22 @@
 import example from "../images/example.jpg";
-import goldenKiwi from "../images/golden-kiwi-insta.jpg";
-import coconut from "../images/coco-insta.jpg"
-import pumpkin from "../images/pumpkin-insta.jpg"
 
 export const instaSlideData = [
     {
         id: 1,
         alt: "photo of orange embroidery",
-        img: goldenKiwi,
+        img: example,
         link: "instagram url here"
     },
     {
         id: 2,
         alt: "photo of orange embroidery",
-        img: coconut,
+        img: example,
         link: "instagram url here"
     },
     {
         id: 3,
         alt: "photo of orange embroidery",
-        img: pumpkin,
+        img: example,
         link: "instagram url here"
     },
     {
