7115fde1f0d249ef11f17066ec64eb028aca72ea
Mattie Barton <maitlandbarton@gmail.com>
2023-05-18 14:34:51 +0200

    edit wording of about me bio

--- src/data/itemData.js
+++ src/data/itemData.js
@@ -5,43 +5,73 @@ export const itemData = [
     {
         id: 1,
         img: coconut,
-        name: 'Citrussy - orange',
-        description: 'hand embroidered fruit vulva in a 3.5in hoop',
+        name: 'Coconut Hand Embroidery',
+        description: 'hand-embroidered coconut vulva in a 3.5in painted hoop.',
         price: 48,
+        category: "hoop art"
     },
     {
         id: 2,
         img: example,
-        name: 'Citrussy - grapefruit',
-        description: 'hand embroidered fruit vulva in a 3.5in hoop',
+        name: 'Papaya Hand Embroidery',
+        description: 'hand-embroidered papaya vulva in a 3.5in stained hoop',
         price: 48,
+        category: "hoop art"
     },
     {
         id: 3,
         img: example,
-        name: 'Pomegranate',
-        description: 'hand embroidered fruit vulva in a 3.5in hoop',
+        name: 'Guava Hand Embroidery',
+        description: 'hand-embroidered guava vulva in a 3.5in stained hoop',
         price: 48,
+        category: "hoop art"
     },
     {
         id: 4,
         img: example,
-        name: 'Coconussy',
-        description: 'hand embroidered fruit vulva in a 3.5in hoop',
+        name: 'Peach Sticker',
+        description: 'vinyl peach vulva sticker, clear outline',
         price: 48,
+        category: "sticker"
     },
     {
         id: 5,
         img: example,
-        name: 'Papaya',
-        description: 'hand embroidered fruit vulva in a 3.5in hoop',
+        name: 'Avocado Sticker',
+        description: 'vinyl avocado vulva sticker, clear outline',
         price: 48,
+        category: "sticker"
     },
     {
         id: 6,
         img: example,
-        name: 'Kiwi',
-        description: 'hand embroidered fruit vulva in a 3.5in hoop',
+        name: 'Pomegranate Sticker',
+        description: 'vinyl pomegranate vulva sticker, clear outline',
         price: 48,
+        category: "sticker"
+    },
+    {
+        id: 7,
+        img: example,
+        name: 'Citrus Embroidery Guide',
+        description: 'DIGITAL DOWNLOAD: 5 in 1 citrus fruit embroidery guide',
+        price: 48,
+        category: "guide"
+    },
+    {
+        id: 8,
+        img: example,
+        name: 'Pumpkin Embroidery Guide',
+        description: 'DIGITAL DOWNLOAD: pumpkin embroidery guide',
+        price: 48,
+        category: "guide"
+    },
+    {
+        id: 9,
+        img: example,
+        name: 'Dragonfruit Embroidery Guide',
+        description: 'DIGITAL DOWNLOAD: dragonfruit embroidery guide',
+        price: 48,
+        category: "guide"
     },
 ]
