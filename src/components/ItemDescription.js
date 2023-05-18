b6951f1d1debfdc9572bca73d6978010c26b16fb
Mattie Barton <maitlandbarton@gmail.com>
2023-05-18 15:30:40 +0200

    responsive design item details page

--- src/components/ItemDescription.js
+++ src/components/ItemDescription.js
@@ -13,15 +13,21 @@ function ItemDescription({ id }) {
           className="md:max-w-xl mx-auto rounded-xl h-auto md:h-4/5 ring-4 ring-orange"
         ></img>
       </div>
-      <div className="md:w-full bg-teal m-10 rounded-xl ring-4 ring-lime">
+      <div className="md:w-full bg-teal m-10 rounded-xl h-full ring-4 ring-lime">
         <div className="ml-5">
           <h1 className="text-5xl mt-5 text-white">{item[index].name}</h1>
           <h2 className="text-3xl text-white">€{item[index].price}</h2>
-          <p>quantity</p>
-          <p>{item[index].description}</p>
-          <p className="mx-auto p-4 bg-navy w-80 text-center text-white rounded-3xl text-xl hover:shadow-lg hover:shadow-lime">
-            Add to Cart
-          </p>
+          <div className="my-5">
+          <p className="text-white text-lg">quantity</p>
+          </div>
+          <div className="my-10">
+          <p className="text-white text-xl">{item[index].description}</p>
+          </div>
+          <div className="m-5">
+            <p className="mx-auto p-4 bg-navy sm:w-1/3 md:w-1/4 lg:w-80 text-center text-white rounded-3xl text-xl hover:shadow-lg hover:shadow-lime">
+              Add to Cart
+            </p>
+          </div>
         </div>
       </div>
     </div>
