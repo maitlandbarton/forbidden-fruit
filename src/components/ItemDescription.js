478a12109b2e59d9eb346e2cd23f5d9254775ca7
Mattie Barton <maitlandbarton@gmail.com>
2023-05-22 18:43:10 +0200

    responsive design item description

--- src/components/ItemDescription.js
+++ src/components/ItemDescription.js
@@ -8,13 +8,13 @@ function ItemDescription({ id }) {
 
   return (
     <div className="flex w-screen flex-col md:flex-row justify-around pt-10">
-      <div id="item-image" className="w-full p-10">
+      <div id="item-image" className="sm:w-full md:w-1/2 h-auto p-10">
         <img
           src={item[index].img}
-          className="md:max-w-xl mx-auto rounded-xl h-auto md:h-4/5 ring-4 ring-orange"
+          className="lg:max-w-xl mx-auto rounded-xl h-auto md:h-4/5 ring-4 ring-orange"
         ></img>
       </div>
-      <div className="md:w-full bg-teal m-10 rounded-xl h-full ring-4 ring-lime">
+      <div className="md:w-1/2 lg:w-full bg-teal m-10 rounded-xl h-full ring-4 ring-lime">
         <div className="ml-5">
           <h1 className="text-5xl mt-5 text-white">{item[index].name}</h1>
           <h2 className="text-3xl text-white">€{item[index].price}</h2>
@@ -25,7 +25,7 @@ function ItemDescription({ id }) {
           <p className="text-white text-xl">{item[index].description}</p>
           </div>
           <div className="m-5">
-            <p className="mx-auto p-4 bg-navy sm:w-1/3 md:w-1/4 lg:w-80 text-center text-white rounded-3xl text-xl hover:shadow-lg hover:shadow-lime">
+            <p className="mx-auto p-4 bg-navy sm:w-1/3 md:w-1/2 lg:w-80 text-center text-white rounded-3xl text-xl hover:shadow-lg hover:shadow-lime">
               Add to Cart
             </p>
           </div>
